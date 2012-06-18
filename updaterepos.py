583a31d074dfc193a9b5a8db407903bee2b60c95
Chris YT Huang <ythuang@gmail.com>
2012-06-18 10:59:21 +0800

    update updaterepos.py script 1. add docstrings 2. rename functions to better describe task

--- updaterepos.py
+++ updaterepos.py
@@ -1,24 +1,45 @@
 #!/usr/bin/env python
-
+"""
+update all git repositories from current folder
+supports git and git-svn repositories
+"""
 import os
 import subprocess
 
-def getdirectories(path): 
-  flist = os.listdir(path)
-  directories = [ f for f in flist if os.path.isdir(os.path.join(path,f)) ]
-  return directories
+def list_directories(path):
+    """
+    list folders in current path
+    """
+    dir_list = os.listdir(path)
+    directories = [f for f in dir_list if os.path.isdir(os.path.join(path, f))]
+    return directories
 
-def findgit(path):
-  os.chdir(path)
-  print "Looking at: " + path
-  dlist = getdirectories(path)
-  if ".git" in dlist:
-    subprocess.call(["git", "pull"])
-  else:
-    for d in dlist:
-      p = os.path.join(path,d)
-      findgit(p)
-  os.chdir("..")
 
-cwd = os.getcwd()
-findgit(cwd)
+def get_command():
+    """
+    get command based on folder type
+    returns either command for git or git-svn
+    """
+    return ["git", "pull"]
+
+
+def find_git_repos(path):
+    """
+    check whether the current folder is a git or git-svn repository
+    update the git or git-svn repository if found
+    """
+    os.chdir(path)
+    print "Looking at: " + path
+    directories = list_directories(path)
+    if ".git" in directories:
+        cmd = get_command()
+        subprocess.call(cmd)
+    else:
+        for d in directories:
+            p = os.path.join(path, d)
+            find_git_repos(p)
+    os.chdir("..")
+
+if __name__ == "__main__":
+    cwd = os.getcwd()
+    find_git_repos(cwd)
