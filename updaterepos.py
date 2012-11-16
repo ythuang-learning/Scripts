4fc0664f8af1184c375500351e837f9e95ff9617
Chris YT Huang <ythuang@gmail.com>
2012-11-16 16:48:52 +0800

    1. update flow, get repo list first 2. support for bare repos

--- updaterepos.py
+++ updaterepos.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 update all git repositories from current folder
-supports git and git-svn repositories
+supports bare git and git-svn repositories
 """
 import os
-#import subprocess
 from gevent.pool import Pool
 from gevent.subprocess import Popen, PIPE, STDOUT
 
 worker_number = 40
 pool = Pool(worker_number)
-repo_count = 0
+git_sign = ".git"
 
 
 def list_directories(path):
@@ -23,21 +23,51 @@ def list_directories(path):
     return directories
 
 
-def get_command():
+def get_command(bare, path):
     """
     get command based on folder type
-    returns either command for git or git-svn
+    returns either command for bare git, standard git or git-svn
     """
-    os.chdir(".git")
-    directories = list_directories(os.getcwd())
-    cmd = ["git", "pull"]
+
+    if(bare):
+        cmd = ["git", "fetch"]
+        return cmd
+
+    directories = list_directories(os.path.join(path, git_sign))
+
     if "svn" in directories:
         cmd = ["git", "svn", "rebase"]
-    os.chdir("..")
+    else:
+        cmd = ["git", "pull"]
+
     return cmd
 
 
-def update_repo(path, cmd, count):
+def is_bare(path):
+    p = os.path.basename(path)
+    return (len(p) > 4) & (p.endswith(git_sign))
+    pass
+
+
+def update_repos(repo_list):
+    """
+    Update repository list
+    """
+
+    repo_count = 0
+
+    for repo in repo_list:
+        bare, path = repo
+        cmd = get_command(bare, path)
+        # print repo_count,"]==> ", repo, "cmd=> ", cmd
+        pool.spawn(update_repo, repo, cmd, repo_count)
+        repo_count += 1
+
+
+def update_repo(repo, cmd, count):
+    bare, path = repo
+
+    os.chdir(path)
     print count, "]", "updating:", path
     p = Popen(' '.join(cmd), cwd=path, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
     for line in p.stdout:
@@ -45,28 +75,37 @@ def update_repo(path, cmd, count):
             print count, "]", path, ":", line,
 
 
-def find_git_repos(path):
+def find_repos(path):
     """
-    check whether the current folder is a git or git-svn repository
-    update the git or git-svn repository if found
+    Find list of potential repositories in current and child folders
     """
+
+    repolist = []
     global repo_count
     os.chdir(path)
-    #print "Looking at: " + path
     directories = list_directories(path)
-    if ".git" in directories:
-        cmd = get_command()
-        repo_count += 1
-        pool.spawn(update_repo, path, cmd, repo_count)
 
-    else:
-        for d in directories:
-            p = os.path.join(path, d)
-            find_git_repos(p)
-    os.chdir("..")
+    # is this a standard git/git-svn repo?
+    if git_sign in directories:
+        repolist.append((False, path))
+        return repolist
+
+    # find git bare repos
+    for d in directories:
+        p = os.path.join(path, d)
+        bare = is_bare(d)
+        if bare:
+            repolist.append((bare, p))
+        else:
+            repolist.extend(find_repos(p))
+
+    return repolist
 
 if __name__ == "__main__":
     cwd = os.getcwd()
-    find_git_repos(cwd)
-    pool.join()
-    print "Processed ", repo_count, " repositories"
+    repos = find_repos(cwd)
+    update_repos(repos)
+    os.chdir(cwd)
+
+    # pool.join()
+    print "Processed ", len(repos), " repositories"
